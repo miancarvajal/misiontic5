c35720b990bb9f70cadce86b38e5c7268d883542
miancarvajal <62520286+miancarvajal@users.noreply.github.com>
2021-10-20 21:00:25 -0500

    se crea proyecto vue, se deja archivo de comandos de la clase

---
+++ appvue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="header">
+		<h3>MEJCD CORP</h3>
+	</div>
+  <img alt="Vue logo" src="./assets/evolucion.jpg">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ appvue/src/components/HelloWorld.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <section class="forms">
+		<div class="body">    
+		<form name = "formulario" id="formulario" method="POST" action="">      
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">              
+						Nombres:
+					</div>
+					<div class="col-md-6">              
+						<input type="text" maxlength="40" name="" id="nombre" size="15" onkeypress="return blqnum(event);"/>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">              
+						Apellidos:
+					</div>
+					<div class="col-md-6">              
+						<input type="text" maxlength="40" name="" id="apellido" size="15" onkeypress="return blqnum(event);"/>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">              
+						Edad:
+					</div>
+					<div class="col-md-6">              
+						<input type="number" name="edad" id="edad" min="0" max="100" onkeypress="return blqlet(event);"/>
+					</div>
+				</div>
+				<div name="medidas">
+					<div class="row">
+						<div class="col-md-6">              
+							Peso (kg):
+						</div>
+						<div class="col-md-6">              
+							<input type="number" name="" id="peso" min="0" max="300" onkeypress="return blqlet(event);"/>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6">              
+							Talla (cm):
+						</div>
+						<div class="col-md-6">              
+							<input type="number" name="talla" id="talla" min="15" max="255" onkeypress="return blqlet(event);"/>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-12"> 
+					<input name="enviar" type="submit" align="centre" id ="enviar" value="Enviar" onclick="controller()"/>
+				</div>
+			</div>
+         </form>
+		</div>
+	</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.header{
+    text-align: center;
+    position: relative;
+    width: 100%;
+    height: 35px;
+    clear:both;
+    background-color: #0a102e;
+    font-family:'century gothic';
+    color: white;
+    
+}
+
+.nav{
+    width: 100%;
+
+}
+
+img{
+    margin: auto;
+    display: block;
+}
+
+.forms{
+    width: 400px;
+    background: #24303c;
+    padding: 30px;
+    margin: auto;
+    margin-top: 10px;
+    border-radius: 4px;
+    font-family: 'calibri';
+    color: white;
+
+}
+
+.medidas{
+	width: 10%;
+
+}
+.edad{
+	width: 40%;
+}
+
+</style>
